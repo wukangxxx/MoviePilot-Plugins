dd1d6acc077297dc3a082ae11aef22b489935c61
Hermes Developer <developer@localhost>
2026-09-20 18:33:02 +0800

    feat(p115subsearch): add Pinglian client

--- plugins.v2/p115subsearch/clients/__init__.py
+++ plugins.v2/p115subsearch/clients/__init__.py
@@ -1,29 +1,15 @@
-"""
-客户端模块
-包含115网盘、PanSou、Nullbr、癫影（Dian115）等客户端
-"""
+"""客户端模块"""
 from .p115 import P115ClientManager
 from .pansou import PanSouClient
 from .nullbr import NullbrClient
 from .kdocs import KDocsClient, KDocsError
 from .dian115 import Dian115Client, Dian115Error
-from .leaderboard import (LeaderboardClient, LeaderboardError, LEADERBOARD_SOURCES,
-                          DEFAULT_LEADERBOARD_SOURCES, default_leaderboard_sources,
-                          normalize_source_ids, resolve_source_ids)
+from .pinglian import PinglianClient
+from .leaderboard import (LeaderboardClient, LeaderboardError, LEADERBOARD_SOURCES, DEFAULT_LEADERBOARD_SOURCES, default_leaderboard_sources, normalize_source_ids, resolve_source_ids)
 
 __all__ = [
-    "P115ClientManager",
-    "PanSouClient",
-    "NullbrClient",
-    "KDocsClient",
-    "KDocsError",
-    "Dian115Client",
-    "Dian115Error",
-    "LeaderboardClient",
-    "LeaderboardError",
-    "LEADERBOARD_SOURCES",
-    "DEFAULT_LEADERBOARD_SOURCES",
-    "default_leaderboard_sources",
-    "normalize_source_ids",
-    "resolve_source_ids"
+    "P115ClientManager", "PanSouClient", "NullbrClient", "KDocsClient", "KDocsError",
+    "Dian115Client", "Dian115Error", "PinglianClient", "LeaderboardClient", "LeaderboardError",
+    "LEADERBOARD_SOURCES", "DEFAULT_LEADERBOARD_SOURCES", "default_leaderboard_sources",
+    "normalize_source_ids", "resolve_source_ids",
 ]
